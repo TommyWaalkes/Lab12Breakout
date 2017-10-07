81f2cb32af1ffe823ff18f520c17822a9fa7b982
Tommy Waalkes <Tommy@grandcircus.co>
2017-10-07 12:05:08 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHLab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RPSGenerator rps = new RPSGenerator();
+            Rocky r = new Rocky(rps);
+            Console.WriteLine(r.GenerateRPS());
+        }
+    }
+}
